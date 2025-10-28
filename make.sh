e9fa7d34e325b3d35e462c1a30b0147bc55078f5
MircaMircas <mikosmikus@gmail.com>
2025-10-28 17:14:38 +0100

    Update make.sh to use s31103built.bin

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if test -f s3built.bin; then
-	mv -f s3built.bin s3built.prev.bin
+if test -f s31103built.bin; then
+	mv -f s31103built.bin s31103built.prev.bin
 fi
 ./tool/linux/asl -xx -q -A -L -U -E -i . main.asm
-./tool/linux/p2bin -p=FF -z=0,uncompressed,Size_of_driver_guess,after main.p s3built.bin
+./tool/linux/p2bin -p=FF -z=0,uncompressed,Size_of_driver_guess,after main.p s31103built.bin
 rm -f main.p
