7f466644046a2ce4f43ebcbb93d9dcd7066f1ad3
MircaMircas <mikosmikus@gmail.com>
2025-10-28 17:14:03 +0100

    Update make.bat to use s31103built.bin

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 @echo off
-IF EXIST s3built.bin move /Y s3built.bin s3built.prev.bin >NUL
+IF EXIST s31103built.bin move /Y s31103built.bin s31103built.prev.bin >NUL
 tool\windows\asw -xx -q -A -L -U -E -i . main.asm
-tool\windows\p2bin -p=FF -z=0,uncompressed,Size_of_driver_guess,after main.p s3built.bin
+tool\windows\p2bin -p=FF -z=0,uncompressed,Size_of_driver_guess,after main.p s31103built.bin
 del main.p
